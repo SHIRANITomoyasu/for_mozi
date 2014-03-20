95a5a3c3bb34c05e5b893814abd21b16a876bb26
shirani <shirani4116@gmail.com>
2014-03-20 18:51:12 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var DEV_PORT = "/dev/cu.usbmodemfd121";
+
+var analogPin = 12;
+
+var offtime = 500;
+
+function beep() {
+    var arduino = document.arduino;
+
+    var sum = 0;
+    for(var i=0; i<100; i++) {
+        sum += arduino.analogRead(analogPin);
+    }
+
+    var val = sum / 100;
+
+    document.getElementById("distance").innerHTML = "<br>" + val + "<br>";
+
+    var ontime = 500;
+
+    if(val > 600) ontime = 523;
+    else if(val > 530) ontime = 494;
+    else if(val > 460) ontime = 440;
+    else if(val > 390) ontime = 392;
+    else if(val > 320) ontime = 349;
+    else if(val > 250) ontime = 330;
+    else if(val > 180) ontmie = 294;
+    else if(val > 110) ontime = 262;
+
+    arduino.pulse(analogPin, ontime, offtime);
+
+    setTimeout("beep()", step);
+}
+
+function setup() {
+    try{
+        with(document.arduino) {
+            open(DEV_PORT);
+            pinMode(analogPin, true);
+    } catch(e) {
+        alert("Oops, an error happened!");
+    }
+}
+
+$(function() {
+    if(!document.arduino) {
+        alert("arduino.js for webpages add-on is not installed.");
+    } else {
+        setup();
+        beep();
+    }
+});
